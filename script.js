236983390d7fa0dba0367fde71b897a82fc62003
Lam Nguyen <156275068+CunOwO@users.noreply.github.com>
2024-12-23 19:35:42 +0700

    feat: enhance form validation and improve book addition
    - Add form validation to ensure required fields are filled before submission.
    - Include logic to handle empty "pages" input by assigning "Unknown" if left blank.
    - Clear form inputs after adding a new book to the library.
    - Ensure dialog closes properly after adding a new book.

--- script.js
+++ script.js
@@ -42,8 +42,33 @@ addNewBook(hobbit, "./book-images/the-hobbit.jpg");
 addNewBook(mockingbird, "./book-images/to-kill-a-mocking-bird.jpg");
 addNewBook(warAndPeace, "./book-images/war-and-peace.jpg");
 
-// const addBook = document.querySelector("button");
-// addBook.addEventListener("click", () => {
-//   const newBook = new Book("djfksl", "fdsalkjdf" ,"fdas", "fdsaf");
-//   addNewBook(newBook);
-// });
+const dialog = document.querySelector("dialog");
+const addBtn = document.querySelector(".addBtn");
+const form = document.querySelector('form');
+const confirmBtn = document.querySelector(".confirmBtn");
+
+addBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+confirmBtn.addEventListener("click", (e) => {
+  if (!form.checkValidity()) { 
+    form.reportValidity(); 
+    return; 
+  }
+  
+  e.preventDefault();
+  const data = Object.fromEntries(new FormData(form).entries());
+  const isTrue = (data.read === "true");
+  if (data.pages === "") {
+    data.pages = "Unknow";
+  }
+  else {
+    data.pages = parseInt(data.pages);
+  }
+  const newBook = new Book(data.title, data.author, data.pages, isTrue);
+  
+  addNewBook(newBook);
+  dialog.close();
+  form.reset();
+});
